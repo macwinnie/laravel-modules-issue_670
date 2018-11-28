32c60d0e91ce60b6a4cb216d37b58abffc9c4581
Martin Winter <git@macwinnie.me>
2018-11-28 10:04:05 +0100

    new module `SubDomain` created by `php artisan module:make SubDomain` + add PSR-4-snippet to `composer.json`

---
+++ Modules/SubDomain/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('subdomain::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('subdomain.name') !!}
+    </p>
+@stop
